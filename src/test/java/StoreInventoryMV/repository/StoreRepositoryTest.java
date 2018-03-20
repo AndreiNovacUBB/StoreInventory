78959b90e4d9188c0423b5830931f0153d050702
Andrei <apple.andrei@gmail.com>
2018-03-20 19:37:46 +0200

    Added tests

---
+++ src/test/java/StoreInventoryMV/repository/StoreRepositoryTest.java
@@ -0,0 +1,26 @@
+package StoreInventoryMV.repository;
+
+import junit.framework.TestCase;
+
+public class StoreRepositoryTest extends TestCase {
+
+	public void testGetAllProducts() {
+	}
+
+	public void testReadFile() {
+		assertTrue(true);
+	}
+
+	public void testAddNewProduct() {
+		assertTrue(true);
+	}
+
+	public void testGetProductsCategory() {
+	}
+
+	public void testStockSituationProduct() {
+	}
+
+	public void testStockSituation() {
+	}
+}
